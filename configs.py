e418dbfaff669eccaf739cb18a67d9d32b88d9f4
Cartierprueba <126520389+Cartierprueba@users.noreply.github.com>
2023-02-28 11:21:55 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,1 +1,9 @@
-from
+from distutils.command.config import config
+from os import path, getenv
+
+class Config:
+    API_ID = int(getenv('API_ID','21199736'))
+    API_HASH = getenv('API_HASH','3ed76d05d92a5203ca076066146a47bc')
+    BOT_TOKEN = getenv('BOT_TOKEN','123456789:AAF8-1234-1234-1234-1234567890AB')
+
+config = Config()
